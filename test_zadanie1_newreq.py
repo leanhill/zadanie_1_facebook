cd4b5be61d08f217ad2bcb64f50325ed5a6cabf4
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-07-20 22:23:09 +0200

    Test z parametryzacja

--- test_zadanie1_newreq.py
+++ test_zadanie1_newreq.py
@@ -1,10 +1,11 @@
 from zadanie1_newreq import get_facebook_likeit_description
+import pytest
 
 
-def test():
-    assert get_facebook_likeit_description(["Piotr"]) == "Piotr lubi to!"
-    assert get_facebook_likeit_description(["Piotr", "Magda"]) == "Piotr i Magda lubią to!"
-    assert get_facebook_likeit_description(["Piotr", "Magda", "Janusz"]) == "Piotr, Magda i Janusz lubią to!"
-    assert get_facebook_likeit_description(["Piotr", "Magda", "Janusz", "Krzysztof"]) == "Piotr, Magda i 2 " \
-                                                                                         "inne osoby lubią to!"
-    assert get_facebook_likeit_description([]) == "Nikt tego nie lubi"
+@pytest.mark.parametrize("n, result", [(["Piotr"], "Piotr lubi to!"), (["Piotr", "Magda"], "Piotr i Magda lubią to!"),
+                                       (["Piotr", "Magda", "Janusz"], "Piotr, Magda i Janusz lubią to!"),
+                                       (["Piotr", "Anna", "Janusz", "Krzysztof"], "Piotr, Anna i 2 inne osoby lubią to!"),
+                                       ([], "Nikt tego nie lubi")])
+
+def test(n, result):
+    assert get_facebook_likeit_description(n) == result
